6bddf1760c572549b06a1e81fd3731df6f1c62a0
Its Subu <79294479+IND-Subu@users.noreply.github.com>
2023-06-23 16:38:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'navbar/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+    <div class="box border rounded col-lg-4 col-sm-4">
+        <p>
+            <h5>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi unde id culpa, aperiam, placeat perspiciatis velit soluta, vel provident.
+            </h5>
+        </p>
+        <div class="buttons">
+            <input class="btn btn-primary btn-sm login" type="button" value="Login">
+            <input class="btn btn-primary btn-sm signup" type="button" value="Sign Up">
+        </div>
+    </div>
+    <script src="js/index.js"></script>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,144 @@
+<?php include 'navbar/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+    <link rel="stylesheet" href="css/product.css">
+</head>
+<body>
+    <div class="section">
+        <div class="container">
+            <div class="btn btn-primary btn-md mt-4 my-2">Catagory</div>
+            <div class="btn btn-success btn-md mt-4 my-2">My Cart (0)</div>
+        </div>
+        <div class="container">
+            <div class="row my-3 gy-3">
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <button class="btn btn-primary">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="src/patato.jpg" alt="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Demo</h5>
+                            <p class="card-text">Rs.25<br>1 Kg</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer border border-success bg-info text-center mt-5">hello</div>
+</body>
+</html>
